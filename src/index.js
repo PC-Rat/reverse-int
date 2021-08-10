9a1139d885366bbc0de8dce02b8c1bcf8a3b7249
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-10 09:47:58 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
 }
+
+function reverse(n) {
+
+	let nAbs = Math.abs(n);
+
+	nAbs = toString(nAbs);
+	let arr = Array.from(nAbs);
+	for (let i = arr.length - 1; i--; i > 0) {
+		let rev = arr.split("").reverse().join("");
+	}
+	return rev;
+}
