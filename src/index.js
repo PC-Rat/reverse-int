f50821232cad2bb3e9bb0361a5a113c77ac4787a
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-10 10:26:35 +0300

    refactor: typing style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 module.exports = function reverse (n) {
+    	n = Math.abs(n);
+	return n.toString().split('')
+		.reverse()
+		.join('');
   
 }
 
-function reverse(n) {
-
-	let nAbs = Math.abs(n);
-
-	nAbs = toString(nAbs);
-	let arr = Array.from(nAbs);
-	for (let i = arr.length - 1; i--; i > 0) {
-		let rev = arr.split("").reverse().join("");
-	}
-	return rev;
-}
